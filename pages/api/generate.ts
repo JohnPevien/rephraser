89ad1880225247f011cb1bbdc507245a988af640
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-09-25 18:17:12 +0800

    update prompt

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -18,7 +18,7 @@ export default async function handler(req: Request): Promise<Response> {
     const vibe = body.vibe;
     const sentence = body.sentence;
 
-    const systemPrompt = `You are a helpful grammar assistant that will correct grammar and generate 5 rephrased sentence in ${vibe} tone. You must not provide other text but only provide the rephrased sentence in points using "-". format and do not use a numbered format. You will not ask but always rephrase what is provided by the user. Separate each number using "/\n/"`;
+    const systemPrompt = `You're a grammar assistant that will correct grammar and generate 5 rephrased sentence in ${vibe} tone and in a totally correct grammar. You must not provide other text but only provide the rephrased sentence in points using "-". format and never use a numbered format. You will never ask and always rephrased sentences based on the text provided. Separate each number using "/\n/"`;
 
     const stream = await OpenAI(
       'chat',
@@ -31,7 +31,7 @@ export default async function handler(req: Request): Promise<Response> {
           },
           {
             role: 'user',
-            content: `Rephrase this "${sentence}"`,
+            content: `Rephrase this sentence and Separate each sentence using "/\n/" "${sentence}"`,
           },
         ],
         max_tokens: 500,
